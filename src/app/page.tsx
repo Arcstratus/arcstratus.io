2aa7fc45dcf3fab6ebe379bc160c4c11782ef4d9
wenke.lai <wenke.studio@gmail.com>
2025-03-21 16:06:01 +0800

    fead: add theme toggle for dark model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { ThemeToggle } from "@/components/theme-toggle";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Welcome to Arcstratus</h1>
+      <ThemeToggle />
     </main>
   );
 }
