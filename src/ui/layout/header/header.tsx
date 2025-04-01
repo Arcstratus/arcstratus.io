e7bc3837fc445f7bf53069ec05f09747c139f13c
wenke.lai <wenke.studio@gmail.com>
2025-04-01 13:42:38 +0800

    fix: update header styles

--- src/ui/layout/header/header.tsx
+++ src/ui/layout/header/header.tsx
@@ -1,18 +1,48 @@
-import { ThemeToggle } from "@/components/theme-toggle";
+"use client";
+
 import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
 
-interface HeaderProps extends React.ComponentProps<"header"> {
-  className?: string;
-}
+export function Header({ children }: { children: React.ReactNode }) {
+  const [visible, setVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [isMobile, setIsMobile] = useState(false);
 
-const headerClassName =
-  "fixed top-0 z-50 w-full h-16 shadow-md bg-background/90";
-const divClassName = "container mx-auto px-4 py-3 flex between middle gap-4";
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+    setVisible(currentScrollY <= lastScrollY);
+    setLastScrollY(currentScrollY);
+  };
 
-export function Header({ children, className }: HeaderProps) {
+  const checkIsMobile = () => {
+    setIsMobile(window.innerWidth < 768);
+  };
+
+  useEffect(() => {
+    checkIsMobile();
+
+    window.addEventListener("resize", checkIsMobile);
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", checkIsMobile);
+    };
+  }, [lastScrollY]);
+
   return (
-    <header className={cn(headerClassName, className)}>
-      <div className={divClassName}>{children}</div>
+    <header
+      className={cn(
+        "fixed inset-x-0 top-0 z-50 w-full backdrop-blur",
+        // Animation
+        "duration-200",
+        isMobile && !visible ? "-translate-y-20" : "translate-y-0"
+      )}
+    >
+      <div className="mx-auto w-full px-4 xl:max-w-7xl relative">
+        <nav className="flex center py-4 h-16 between gap-4">{children}</nav>
+      </div>
     </header>
   );
 }
