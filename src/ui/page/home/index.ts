31c86ec80f2e2956b9cc361c312a9b2a785607c3
wenke.lai <wenke.studio@gmail.com>
2025-04-02 17:28:45 +0800

    fix(page): remove about section

--- src/ui/page/home/index.ts
+++ src/ui/page/home/index.ts
@@ -1,7 +1,6 @@
-import { About } from "./about";
 import { Hero } from "./hero";
 import { Platform } from "./platform";
 import { Service } from "./service";
 import { ServiceAI } from "./service-ai";
 
-export { About, Hero, Platform, Service, ServiceAI };
+export { Hero, Platform, Service, ServiceAI };
