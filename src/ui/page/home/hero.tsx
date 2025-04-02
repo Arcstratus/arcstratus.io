53c04d4db1359d470a97dccc44c26784487a2840
wenke.lai <wenke.studio@gmail.com>
2025-04-02 15:52:02 +0800

    fix(page): update content for hero

--- src/ui/page/home/hero.tsx
+++ src/ui/page/home/hero.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { getDictionary, type Locale } from "@/i18n/utils";
-import { IconBadge, IconChevronsDown } from "@tabler/icons-react";
+import { IconChevronsDown } from "@tabler/icons-react";
 
 interface HeroProps {
   lang: Locale;
@@ -12,17 +12,14 @@ export async function Hero(props: HeroProps) {
   return (
     <section id="hero" className="relative pt-16 h-svh">
       <div className="container mx-auto h-full flex flex-col">
-        <div className="grid grid-cols-1 gap-8 items-center md:grid-cols-2 grow">
+        <div className="grid grid-cols-1 gap-8  items-center md:grid-cols-2 grow">
           <div className="flex flex-col gap-4">
-            <div>
-              <IconBadge />
-            </div>
             <div className="flex gap-4 flex-col">
               <h1 className="text-5xl md:text-7xl max-w-lg tracking-tighter text-left font-regular">
-                {dict.hero.title}
+                {dict.home.hero.title}
               </h1>
               <p className="text-xl leading-relaxed tracking-tight text-muted-foreground max-w-md text-left">
-                {dict.hero.description}
+                {dict.home.hero.slogan}
               </p>
             </div>
             <div className="flex gap-4">
@@ -38,7 +35,7 @@ export async function Hero(props: HeroProps) {
             <div className="bg-muted rounded-md aspect-square"></div>
           </div>
         </div>
-        <div className="p-8">
+        <div className="absolute bottom-6 right-2 md:relative md:p-8">
           <IconChevronsDown size={32} className="animate-bounce" />
         </div>
       </div>
