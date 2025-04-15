946afc7da2e6f4c236dfe9f5890973824759bfe3
wenke.lai <wenke.studio@gmail.com>
2025-04-16 05:49:08 +0800

    fix(component): set target attr to 3d-ping link

--- src/components/aceternity/3d-ping.tsx
+++ src/components/aceternity/3d-ping.tsx
@@ -40,6 +40,7 @@ export const PinContainer = ({
         onMouseEnter={onMouseEnter}
         onMouseLeave={onMouseLeave}
         href={href || "/"}
+        target={href?.startsWith("http") ? "_blank" : undefined}
       >
         <div
           style={{
@@ -76,8 +77,8 @@ export const PinPerspective = ({
         <div className="absolute top-0 inset-x-0 flex justify-center">
           <a
             href={href}
-            target={"_blank"}
-            className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10"
+            target={href?.startsWith("http") ? "_blank" : undefined}
+            className="relative flex space-x-2 items-center rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10"
           >
             <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
               {title}
