93ca513ade28ecbec514444392495ae2684e3ab5
wenke.lai <wenke.studio@gmail.com>
2025-04-15 13:15:03 +0800

    fix(page): refactor sections for news page

---
+++ src/ui/page/news/index.ts
@@ -0,0 +1,5 @@
+import { LatestNews } from "./latest-news";
+import { Milestone } from "./milestone";
+import { Products } from "./products";
+
+export { LatestNews, Milestone, Products };
---
+++ src/ui/page/news/latest-news.tsx
@@ -0,0 +1,14 @@
+export const LatestNews = () => {
+  return (
+    <section className="relative w-full">
+      <h1 className="capitalize">latest news</h1>
+      <h2>2025 4</h2>
+      <p>
+        We're about to launch our first product, BuyForYou, a shopping assistant
+        for every family that makes purchasing easier and more accurate. Upload
+        photos, record voice notes, and sync shopping lists effortlessly. Even
+        "Panic Dad" can instantly transform into a shopping pro and team MVP.
+      </p>
+    </section>
+  );
+};
