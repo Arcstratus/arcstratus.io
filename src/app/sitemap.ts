dc875be3447d2427c93b55e4d71bfd8f13825905
wenke.lai <wenke.studio@gmail.com>
2025-04-16 09:48:13 +0800

    feat(sitemap): add news and products URLs with localization options

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -14,5 +14,29 @@ export default function sitemap(): MetadataRoute.Sitemap {
         },
       },
     },
+    {
+      url: "https://arcstratus.io/news",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+      alternates: {
+        languages: {
+          en: "https://arcstratus.io/en/news",
+          tw: "https://arcstratus.io/zh/news",
+        },
+      },
+    },
+    {
+      url: "https://arcstratus.io/products",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.4,
+      alternates: {
+        languages: {
+          en: "https://arcstratus.io/en/products",
+          tw: "https://arcstratus.io/zh/products",
+        },
+      },
+    },
   ];
 }
