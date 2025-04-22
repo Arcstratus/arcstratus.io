2f58957e20655b9eb585ec674131c3e48722290a
wenke.lai <wenke.studio@gmail.com>
2025-04-22 21:58:40 +0800

    fix: exclude robots and sidemap from middleware redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,14 @@
-import { NextRequest } from "next/server";
+import { defaultLocale, locales } from "@/i18n/utils";
 import { match } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
-import { locales, defaultLocale } from "@/i18n/utils";
+import { NextRequest } from "next/server";
 
 function getLocale(request: NextRequest): string {
   // Negotiator expects request headers to be plain objects
   const headers = Object.fromEntries(request.headers.entries());
   const negotiator = new Negotiator({ headers });
   const languages = negotiator.languages();
-  
+
   // Try to match the user's preferred language with our supported locales
   try {
     return match(languages, locales, defaultLocale);
@@ -19,24 +19,24 @@ function getLocale(request: NextRequest): string {
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  
+
   // Check if the pathname already has a locale
   const pathnameHasLocale = locales.some(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
   );
-  
+
   if (pathnameHasLocale) return;
-  
+
   // Redirect if there is no locale
   const locale = getLocale(request);
   request.nextUrl.pathname = `/${locale}${pathname}`;
-  
+
   return Response.redirect(request.nextUrl);
 }
 
 export const config = {
   matcher: [
-    // Skip all internal paths (_next, api, etc)
-    "/((?!_next|api|favicon.ico).*)",
+    // Skip all internal paths (_next, api, etc) and specific files
+    "/((?!_next|api|favicon.ico|robots.txt|sitemap.xml).*)",
   ],
 };
