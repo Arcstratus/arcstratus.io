e0e6ba69e2ea9d8773087e243399acd9c85642f0
wenke.lai <wenke.studio@gmail.com>
2025-05-05 14:25:34 +0800

    fix(component): clear styles for sticky-scroll-reveal

--- src/components/aceternity/sticky-scroll-reveal.tsx
+++ src/components/aceternity/sticky-scroll-reveal.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import { IconArrowDown } from "@tabler/icons-react";
 import { motion, useMotionValueEvent, useScroll } from "motion/react";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
 
 export const StickyScroll = ({
   content,
@@ -19,8 +18,6 @@ export const StickyScroll = ({
   const [activeCard, setActiveCard] = React.useState(0);
   const ref = useRef<any>(null);
   const { scrollYProgress } = useScroll({
-    // uncomment line 22 and comment line 23 if you DONT want the overflow container and want to have it change on the entire page scroll
-    // target: ref
     container: ref,
     offset: ["start start", "end start"],
   });
@@ -41,78 +38,46 @@ export const StickyScroll = ({
     setActiveCard(closestBreakpointIndex);
   });
 
-  const backgroundColors = [
-    "#0f172a", // slate-900
-    "#000000", // black
-    "#171717", // neutral-900
-  ];
-  const linearGradients = [
-    "linear-gradient(to bottom right, #06b6d4, #10b981)", // cyan-500 to emerald-500
-    "linear-gradient(to bottom right, #ec4899, #6366f1)", // pink-500 to indigo-500
-    "linear-gradient(to bottom right, #f97316, #eab308)", // orange-500 to yellow-500
-  ];
-
-  const [backgroundGradient, setBackgroundGradient] = useState(
-    linearGradients[0]
-  );
-
-  useEffect(() => {
-    setBackgroundGradient(linearGradients[activeCard % linearGradients.length]);
-  }, [activeCard]);
-
   return (
     <div className="relative w-full">
       <motion.div
-        animate={{
-          backgroundColor:
-            backgroundColors[activeCard % backgroundColors.length],
-        }}
-        className="relative flex h-[30rem] justify-center space-x-10 overflow-y-auto rounded-md p-10"
+        className="relative h-[30rem] flex justify-center gap-24 overflow-y-auto"
         ref={ref}
       >
-        <div className="div relative flex items-start px-4">
+        {/* Content */}
+        <div className="relative flex items-start py-4 w-1/3">
           <div className="max-w-2xl">
             {content.map((item, index) => (
-              <div key={item.title + index} className="my-20">
+              <div key={item.title} className="my-20">
                 <motion.h2
-                  initial={{
-                    opacity: 0,
-                  }}
-                  animate={{
-                    opacity: activeCard === index ? 1 : 0.3,
-                  }}
-                  className="text-2xl font-bold text-slate-100"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: activeCard === index ? 1 : 0 }}
                 >
                   {item.title}
                 </motion.h2>
                 <motion.p
-                  initial={{
-                    opacity: 0,
-                  }}
-                  animate={{
-                    opacity: activeCard === index ? 1 : 0.3,
-                  }}
-                  className="text-kg mt-10 max-w-sm text-slate-300"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: activeCard === index ? 1 : 0 }}
+                  className="mt-10 max-w-sm"
                 >
                   {item.description}
                 </motion.p>
               </div>
             ))}
-            <div className="h-40" />
+            <div className="h-16" />
           </div>
         </div>
-        <div
-          style={{ background: backgroundGradient }}
-          className={cn(
-            "sticky top-10 hidden h-60 w-80 overflow-hidden rounded-md bg-white lg:block",
-            contentClassName
-          )}
-        >
-          {content[activeCard].content ?? null}
+
+        {/* Screenshot */}
+        <div className="sticky inset-0 hidden lg:block overflow-hidden w-1/3 h-full">
+          <div className="flex items-center justify-center h-full">
+            {content[activeCard].content ?? null}
+          </div>
         </div>
       </motion.div>
+
       <div className="absolute bottom-8 right-8 ">
-        <IconArrowDown className="size-8 text-background dark:text-foreground animate-bounce" />
+        <IconArrowDown className="size-8 text-primary/60 animate-bounce" />
       </div>
     </div>
   );
